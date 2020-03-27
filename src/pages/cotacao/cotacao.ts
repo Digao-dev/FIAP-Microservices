13f74b8df75327bba011033c1d9b0082fa6748ba
Marina Cassiolli <marinacassiolli@live.com>
2020-03-26 23:29:02 -0300

    Chamada para o microserviço funcionando

--- src/pages/cotacao/cotacao.ts
+++ src/pages/cotacao/cotacao.ts
@@ -2,39 +2,38 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 
-/**
- * Generated class for the CotacaoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-cotacao',
   templateUrl: 'cotacao.html',
 })
 export class CotacaoPage {
-
   public quantidade: number;
+  public API_URL = 'http://192.168.99.100:8080/'; // IP do Docker
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public http: HttpClient) { }
 
-  getCotacao() {
-    let data = JSON.stringify({
-      'moeda': 'USD',
-      'valorVendaMoeda': 5.0,
+  getCotacao = function() {
+    let body = JSON.stringify({
+      'moeda': 'USD', // valor fixo pois seria outro microserviço
+      'valorVendaMoeda': 5.0, // valor fixo pois seria outro microserviço
       'quantidade': this.quantidade
     });
 
-     // nada acontece
-    return this.http.post(
-      'http://192.168.99.100:8080/cotacao', 
-      data, 
+
+    this.http.post(
+      this.API_URL + 'conversao', 
+      body, 
       { headers: { 'Content-Type': 'application/json' }})
-      .subscribe(data => console.log("data: " + data));
+    .subscribe(
+      body => {
+        console.log(body) 
+        //this.navCtrl.push("pagina", { response: body })
+      }, 
+      error => { console.log(JSON.stringify(error.json())) }
+    );
   }
 }
